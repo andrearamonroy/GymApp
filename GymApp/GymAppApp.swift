39d69192f4ab6aa3181b16f727e66ba10f982a46
AndreaMonroy <andrearamonroy@gmail.com>
2022-06-05 18:33:34 -0400

    Initial Commit

---
+++ GymApp/GymAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymAppApp.swift
+//  GymApp
+//
+//  Created by Andrea on 6/5/22.
+//
+
+import SwiftUI
+
+@main
+struct GymAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
